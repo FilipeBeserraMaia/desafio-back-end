e3402a21ef5d36b11b9a05ee475d4a0facf418a3
FilipeBeserraMaia <filipeifgcc@gmail.com>
2022-12-20 10:37:42 -0300

    ADDED municipe & endereco basic scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
 
   resources :municipes do
     collection do
@@ -8,8 +9,9 @@ Rails.application.routes.draw do
       get :delete_foto_attached
     end
   end
-  root to: 'pages#home'
 
+
+  resources :enderecos
   get :estados, controller: :brasil_api
   get :municipios, controller: :brasil_api
   get :cep, controller: :brasil_api
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_24_143451) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_20_133232) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "enderecos", force: :cascade do |t|
+    t.string "cep", limit: 8, null: false
+    t.string "longr", limit: 50, null: false
+    t.string "complemento", limit: 50
+    t.string "bairro", limit: 80, null: false
+    t.string "cidade", limit: 30, null: false
+    t.string "uf", limit: 2, null: false
+    t.string "cdg_ibge", limit: 7
+    t.integer "municipe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "municipes", force: :cascade do |t|
+    t.string "nome", limit: 80, null: false
+    t.string "cpf", limit: 11, null: false
+    t.string "cns", limit: 15, null: false
+    t.string "email", limit: 256, null: false
+    t.date "dta_nasc", null: false
+    t.string "telefone", limit: 13, null: false
+    t.integer "status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "enderecos", "municipes"
 end
